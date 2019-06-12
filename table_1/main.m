63fd719fe29db68d9ca1e74e43d489477ec197f8
zhyhuang <zhyhuang@umich.edu>
2019-06-12 11:10:31 -0400

    Add files via upload

---
+++ table_1/main.m
@@ -0,0 +1,112 @@
+    n=11;                 % dimension
+    m=1;                 % number of constraints
+    N_data=120;          % sample size
+    n_outer=1000;        % outer test size
+    delta=0.05;
+    epsilon=0.05;
+
+    rng(123)
+    % LP setting
+    load('c_sigma_for_11.mat') % d11 in paper
+    A=-c'; %
+    [A_r A_c]=size(A);
+    b=  [1200];
+
+    % parameters for data 
+    miu_0=A;
+
+    % setting for RO and Recon
+    B_2=60;              % phase II budget
+    B_1=N_data-B_2;      % phase I budget
+    rank_of_data=binoinv(1-delta,B_2,1-epsilon); % estimated quantile
+    rank_of_data_p1=binoinv(1-delta,B_1,1-epsilon); % estimated quantile for recon phase 1
+
+    % setting for FAST
+    N1_fast=61;
+    N2_fast=59;
+    x_fast_0=zeros(n,1);
+
+    % result record
+    fv_fast=zeros(n_outer,1);
+    fv_sg=zeros(n_outer,1);
+    fv_ro=zeros(n_outer,1);
+    fv_recon=zeros(n_outer,1);
+    fv_mo_dro=zeros(n_outer,1);
+
+    violation_fast=zeros(n_outer,1);
+    violation_sg=zeros(n_outer,1);
+    violation_ro=zeros(n_outer,1);
+    violation_recon=zeros(n_outer,1);
+    violation_mo_dro=zeros(n_outer,1);
+    
+    time_fast=zeros(n_outer,1);
+    time_sg=zeros(n_outer,1);
+    time_ro=zeros(n_outer,1);
+    time_recon=zeros(n_outer,1);
+    time_mo_dro=zeros(n_outer,1);
+
+    for i=1:n_outer
+       dataset=mvnrnd(miu_0,sigma,N_data);
+        
+       %% FAST
+       tic 
+       dataset_fast_1=dataset(1:N1_fast,:);
+       dataset_fast_2=dataset(N1_fast+1:end,:);
+       [x_FAST] = FAST_ccp(dataset_fast_1,dataset_fast_2,c,b,x_fast_0);
+       time_fast(i)=toc;
+       fv_fast(i)=c_t*x_FAST;
+       violation_fast(i)=1-normcdf((b-A*x_FAST)/norm(sqrtm(sigma)*x_FAST));
+       %% SG
+       tic
+       A_gen=reshape(dataset',A_c,N_data*A_r)';
+       [x_SG]=SG_ccp(A_gen,c,b);
+       time_sg(i)=toc;
+       fv_sg(i)=c_t*x_SG;
+       violation_sg(i)=1-normcdf((b-A*x_SG)/norm(sqrtm(sigma)*x_SG));
+       %% RO
+       tic
+       dataset_ro_1=dataset(1:B_1,:);
+       dataset_ro_2=dataset(B_1+1:end,:);
+       [x_RO] = RO_ccp(dataset_ro_1,dataset_ro_2,rank_of_data+1,c,b);
+       time_ro(i)=toc;
+       fv_ro(i)=c_t*x_RO;
+       violation_ro(i)=1-normcdf((b-A*x_RO)/norm(sqrtm(sigma)*x_RO));
+       %% Reconstructed RO
+       tic
+       dataset_recon_1=dataset(1:B_1,:);
+       dataset_recon_2=dataset(B_1+1:end,:);
+       [x_Recon] = Recon_ccp(dataset_recon_1,dataset_recon_2,rank_of_data_p1+1,rank_of_data+1,c,b);
+       time_recon(i)=toc;
+       fv_recon(i)=c_t*x_Recon;
+       violation_recon(i)=1-normcdf((b-A*x_Recon)/norm(sqrtm(sigma)*x_Recon));
+       %% Moment-based DRO
+       tic
+        [x_mo_DRO] = moment_DRO_ccp(dataset,c,b,epsilon,delta)
+        time_mo_dro(i)=toc;
+        fv_mo_dro(i)=c_t*x_mo_DRO;
+        violation_mo_dro(i)=1-normcdf((b-A*x_mo_DRO)/norm(sqrtm(sigma)*x_mo_DRO));
+    end
+    
+    % ture solution
+    phi_quantile=norminv(1-epsilon,0,1);
+    rt_sigma=sqrtm(sigma);     
+    [x_true] =cvx_closed_one_line(c,phi_quantile,rt_sigma,miu_0,b);
+    fv_true=c'*x_true;
+    
+    result_table=cell(7,6);
+    result_table(1,:)={'','RO','Recon','SG','FAST','DRO Mo'};
+    result_table(2,:)={'n',N_data,N_data,N_data,N_data,N_data};
+    result_table(3,:)={'n1',B_1,B_1,'-',N1_fast,'-'};
+    result_table(4,:)={'n2',B_2,B_2,'-',N2_fast,'-'};
+    result_table(5,:)={'ov',mean(fv_ro),mean(fv_recon),mean(fv_sg),mean(fv_fast),mean(fv_mo_dro)};
+    result_table(6,:)={'eps',mean(violation_ro),mean(violation_recon),mean(violation_sg),mean(violation_fast),mean(violation_mo_dro)};
+    result_table(7,:)={'delta',sum(violation_ro>delta)/n_outer,sum(violation_recon>delta)/n_outer,sum(violation_sg>delta)/n_outer,sum(violation_fast>delta)/n_outer,sum(violation_mo_dro>delta)/n_outer};
+    disp('Results')
+    disp(result_table)
+    
+    disp('True solution')
+    disp(fv_true)
+    
+    computation_time=[mean(time_ro),mean(time_recon),mean(time_sg),mean(time_fast),mean(time_mo_dro)];
+    disp('Average Computation Time')
+    disp(computation_time)
--- table_1/moment_DRO_ccp.m
+++ table_1/moment_DRO_ccp.m
@@ -1,52 +1,80 @@
-function [outputArg1,outputArg2] = moment_DRO_ccp(dataset_points_1,inputArg2)
-%UNTITLED4 此处显示有关此函数的摘要
-%   此处显示详细说明
- triu_ind=triu(true(n));
+function [x_mo_DRO] = moment_DRO_ccp(dataset,c,b,epsilon,delta)
+%[x_mo_DRO] = moment_DRO_ccp(dataset,c,b,epsilon,delta)
+%   moment_based DRO
+
+[N_data,d]=size(dataset);
+
+ triu_ind=triu(true(d));
 %   
- col_num=repmat(1:n,n,1);
+ col_num=repmat(1:d,d,1);
  row_num=col_num';
 %  
  row_ind_use=row_num(triu_ind);
  col_ind_use=col_num(triu_ind);
 %  
 % 
-n_phi_input=n+n*(n+1)/2;
+d_phi_input=d+d*(d+1)/2;
 
-dataset_for_theta=[dataset_points_1, dataset_points_1(:,row_ind_use).*dataset_points_1(:,col_ind_use)];
+dataset_for_theta=[dataset, dataset(:,row_ind_use).*dataset(:,col_ind_use)];
 
     theta_estimate=mean(dataset_for_theta);
-    new_sigma=cov(dataset_for_theta,1);
+    sigma_estimate=cov(dataset_for_theta,1);
 
-   value_jacob= jacobian_phi_origin(theta_estimate);
+   value_jacob= jacobian_phi_origin(theta_estimate,delta);
 
-    V_est=value_jacob*new_sigma*value_jacob';
+    V_est=value_jacob*sigma_estimate*value_jacob';
     V_est_inv=inv(V_est);
 
-    mu_hat=mean(dataset_points_1);
-    Sigma_hat=cov(dataset_points_1,1);
+    mu_hat=mean(dataset);
+    Sigma_hat=cov(dataset,1);
     sqrt_V=sqrtm(V_est_inv);
     R_inv=inv(sqrt_V);
-    rho=sqrt(chi2inv(1-beta,n_phi_input)/N_data);
-    tilde_c=-b1;
+    rho=sqrt(chi2inv(1-delta,d_phi_input)/N_data);
+    tilde_c=-b;
 
-    svec_operator=sqrt(2)*ones(n)-sqrt(2)*eye(n)+eye(n);
+    svec_operator=sqrt(2)*ones(d)-sqrt(2)*eye(d)+eye(d);
     svec_multiplier=svec_operator(triu_ind);
-    A=[eye(n),zeros(n,n_phi_input-n);
-        zeros(n_phi_input-n,n),diag(svec_multiplier)];
+    A=[eye(d),zeros(d,d_phi_input-d);
+        zeros(d_phi_input-d,d),diag(svec_multiplier)];
 
         cvx_begin
-                    variable x_data_dro(n)
-                    variable W(n,n)
-                    variable vec_q(n_phi_input)
+                    variable x_mo_DRO(d)
+                    variable W(d,d)
+                    variable vec_q(d_phi_input)
                     variable eta
-                    minimize (c'*x_data_dro)
+                    minimize (c'*x_mo_DRO)
                     subject to
-                    sqrt(alpha/(1-alpha))*mu_hat*x_data_dro + trace (Sigma_hat *W) + rho *norm((A*R_inv)'*(vec_q)) +sqrt(alpha/(1-alpha))* tilde_c+eta/4  <= 0
-                    vec_q==[x_data_dro;W(triu_ind).*svec_multiplier];
-                    [W,x_data_dro;x_data_dro',eta] == semidefinite(n+1)
+                    sqrt(epsilon/(1-epsilon))*mu_hat*x_mo_DRO + trace (Sigma_hat *W) + rho *norm((A*R_inv)'*(vec_q)) +sqrt(epsilon/(1-epsilon))* tilde_c+eta/4  <= 0
+                    vec_q==[x_mo_DRO;W(triu_ind).*svec_multiplier];
+                    [W,x_mo_DRO;x_mo_DRO',eta] == semidefinite(d+1)
 
         cvx_end
 
 
 end
+
+function [jac_mat] = jacobian_phi_origin(x,delta)
+%only for alpha=0.05
+n_phi_input=length(x);
+% n_phi_input=n_variable+n_variable*(n_variable+1)/2;
+roos_v=roots([1/2 3/2 -n_phi_input]);
+n_variable=round(roos_v(roos_v>0));
+jac_mat=eye(n_phi_input);
+jac_mat(1:n_variable,1:n_variable)=sqrt(delta/(1-delta))*eye(n_variable);
+row_num=n_variable;
+for i=1:n_variable
+    for j=1:i
+        row_num=row_num+1;
+        if j==i
+            jac_mat(row_num,j)=-2*x(i);
+        else
+            jac_mat(row_num,j)=-x(i);
+            jac_mat(row_num,i)=-x(j);
+        end
+    end
+    
+end
+end
+
+
 
